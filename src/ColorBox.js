05351b881f862452fe7dc34cefb53ea1f7ee2c3a
00010100 <e0001101004@gmail.com>
2019-06-21 10:42:01 +0300

    Styled ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,8 +8,13 @@ export default class ColorBox extends Component {
 
     return (
       <div style={{ background }} className="ColorBox">
-        <span>{name}</span>
-        <span>MORE</span>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     )
   }
