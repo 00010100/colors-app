3bb25b1aa5b2ab547a1650aa450c09fe4044f6a3
00010100 <e0001101004@gmail.com>
2019-06-21 14:52:13 +0300

    Added snackbar and styled select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Select, MenuItem } from '@material-ui/core';
+import { Select, MenuItem, Snackbar, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 export default class Navbar extends Component {
-  state = { format: 'hex' };
+  state = { format: 'hex', open: false };
 
-  handleChange = (e) => {
+  handleFormatChange = (e) => {
     const { value } = e.target;
-    this.setState({ format: value });
+    this.setState({ format: value, open: true });
     this.props.changeFormat(value);
+  };
+  
+  closeSnackbar = () => {
+    this.setState({ open: false });
   };
 
   render() {
     const { level, changeLevel } = this.props;
-    const { format } = this.state;
+    const { format, open } = this.state;
 
     return (
       <header className="Navbar">
@@ -36,12 +41,36 @@ export default class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
-            <MenuItem value="hex">HEX - #fff</MenuItem>
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="hex">HEX - ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
             <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar 
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          onClose={this.closeSnackbar}
+          open={open}
+          autoHideDuration={3000}
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              key="close"
+              color="inherit"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     )
   }
