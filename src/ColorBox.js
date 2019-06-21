437ca4f626e6a21465cf426107674498d7a38923
00010100 <e0001101004@gmail.com>
2019-06-21 17:32:01 +0300

    Displayed colors in SingleColorPalette

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -17,8 +17,10 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    const { background, name, moreUrl } = this.props;
+    const { background, name, moreUrl, showLink } = this.props;
     const { copied } = this.state;
+
+    console.log(showLink);
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -34,9 +36,13 @@ export default class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
-          <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-            <span className="see-more">More</span>
-          </Link>
+          {
+            showLink && (
+              <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+                <span className="see-more">More</span>
+              </Link>
+            )
+          }
         </div>
       </CopyToClipboard>
     )
